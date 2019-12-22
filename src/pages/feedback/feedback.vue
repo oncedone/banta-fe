eb5bd86492a6ad210364d87fe63156cc382c963b
fanlong <814885004@qq.com>
2019-12-22 23:55:07 +0800

    fix

---
+++ src/pages/feedback/feedback.vue
@@ -0,0 +1,32 @@
+<template>
+	<view>
+        <view></view>
+        <view>
+            <view></view>
+            <input />
+        </view>
+        <view>
+            <button></button>
+        </view>
+    </view>
+</template>
+
+<script>
+import { getChannelList, getArticleList, getArticleById } from '@/services/common';
+
+export default {
+    data() {
+        return {};
+    },
+    onLoad() {
+
+    },
+    methods: {
+
+    },
+}
+</script>
+
+<style scoped>
+
+</style>
